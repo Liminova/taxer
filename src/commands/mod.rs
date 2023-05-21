18043a5af60b85e34ffea48dd40425bf58433f17
GHCMaxim <maxim080503@gmail.com>
2023-05-21 23:02:27 +0700

    feat(component): added unsatisfaction, briten, draken

--- src/commands/mod.rs
+++ src/commands/mod.rs
@@ -1,6 +1,10 @@
 use rand::Rng;
+pub mod briten;
+pub mod draken;
 pub mod ping;
 pub mod satisfaction;
+pub mod unsatstifaction;
+
 pub(crate) fn out_of_ten() -> i32 {
     let mut rng = rand::thread_rng();
     rng.gen_range(-1..11)
