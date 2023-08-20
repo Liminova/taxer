12816a9fa3daecdac04ff08011b74112c092696b
GHCMaxim <maxim080503@gmail.com>
2023-08-20 20:49:40 +0700

    fix(add.rs): fixed distance_matrix query and grabbed distance

--- src/commands/add.rs
+++ src/commands/add.rs
@@ -1,45 +1,45 @@
 use crate::{Context, Error};
 use dotenv::dotenv;
+use dotenvy::dotenv;
 use google_maps::prelude::*;
+use sqlite::Connection;
+use std::env;
 
 // Parameters: t!add <address> <name>
 #[poise::command(prefix_command, slash_command, aliases("add"))]
 pub async fn add(ctx: Context<'_>, location: String, name: String) -> Result<(), Error> {
-    let google_maps_client = GoogleMapsClient::new("API_KEY");
+    dotenv().expect("failed to load .env file.");
+    let google_maps_client = GoogleMapsClient::new(&env::var("API_KEY").unwrap());
     let connection = Connection::open("food.db").unwrap();
 
     ctx.defer().await?;
 
-    let query_from_H = google_maps_client
+    let query = google_maps_client
         .distance_matrix(
             vec![Waypoint::Address(String::from(
-                format! {"{}, Hanoi", location},
+                format! {"{}, Hanoi, Vietnam", location},
             ))],
-            vec![Waypoint::Address(String::from(
-                "Hanoi University of Science and Technology",
-            ))],
+            vec![
+                Waypoint::Address(String::from("Hanoi University of Science and Technology")),
+                Waypoint::Address(String::from("AEON MALL Long Bien")),
+            ],
         )
         .with_unit_system(UnitSystem::Metric)
         .execute()
-        .await?;
-    let query_from_LB = google_maps_client
-        .distance_matrix(
-            vec![Waypoint::Address(String::from(
-                format! {"{}, Hanoi", location},
-            ))],
-            vec![Waypoint::Address(String::from("AEON MALL Long Bien"))],
-        )
-        .with_unit_system(UnitSystem::Metric)
-        .execute()
-        .await?;
-    let distance_from_H = query_from_H.rows[0].elements[0]
+        .await
+        .unwrap();
+    let distance_from_hust = query.rows[0].elements[0]
         .distance
+        .clone()
+        .unwrap()
         .text
         .replace(" km", "")
         .parse::<f64>()
         .unwrap();
-    let distance_from_LB = query_from_LB.rows[0].elements[0]
+    let distance_from_lb = query.rows[0].elements[1]
         .distance
+        .clone()
+        .unwrap()
         .text
         .replace(" km", "")
         .parse::<f64>()
