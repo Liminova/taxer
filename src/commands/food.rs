aeb36d67b528231c94f6346e36f3deb1163a3a87
GHCMaxim <maxim080503@gmail.com>
2023-08-20 20:57:57 +0700

    feat(food.rs): finished get name, distance, and location

--- src/commands/food.rs
+++ src/commands/food.rs
@@ -33,5 +33,26 @@ pub async fn food(ctx: Context<'_>, location: String, max_distance: String) -> R
         list.push(row);
     }
     let random = rand::thread_rng().gen_range(0..list.len());
+    let name = list[random].clone();
+    let place = connection
+        .prepare(format!("SELECT Address FROM db WHERE Name = {}", name))
+        .unwrap()
+        .read::<String, _>("Address")
+        .unwrap();
+    let distance_from_place = connection
+        .prepare(format!(
+            "SELECT Distance_from_{} FROM db WHERE Name = {}",
+            location, name
+        ))
+        .unwrap()
+        .read::<f64, _>("Distance")
+        .unwrap();
+    ctx.send(|f| {
+        f.content(format!(
+            "You should go to {} at {} ({} km from {})",
+            name, place, distance_from_place, location
+        ))
+    })
+    .await?;
     Ok(())
 }
