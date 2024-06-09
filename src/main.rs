a0726e735ea922e5385cbc462210da5ffa8bbf21
Delnegend <kiennguyen19323@gmail.com>
2024-06-09 17:27:54 +0700

    refactor(main.rs): new structure for poise

--- src/main.rs
+++ src/main.rs
@@ -20,48 +20,60 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     let framework = poise::Framework::builder()
         .options(FrameworkOptions {
-            commands: vec![
-                commands::ping::ping(),
-                commands::help::help(),
-                commands::qt::qt(),
-            ],
-            on_error: |error| {
-                Box::pin(async move {
-                    match error {
-                        poise::FrameworkError::ArgumentParse { error, .. } => {
-                            if let Some(e) = error.downcast_ref::<serenity::RoleParseError>() {
-                                error!("found a RoleParseError: {:#?}", e);
-                            } else {
-                                error!("not a RoleParseError: {:#?}", error);
-                            }
-                        }
-                        other => {
-                            if let Err(e) = poise::builtins::on_error(other).await {
-                                error!("fatal error: {}", e);
-                            }
-                        }
-                    }
-                })
-            },
             prefix_options: poise::PrefixFrameworkOptions {
                 prefix: Some("t!".into()),
                 case_insensitive_commands: true,
                 ..Default::default()
             },
+            commands: vec![
+                commands::ping::ping(),
+                commands::help::help(),
+                commands::qt::qt(),
+                commands::qt::qt_cm(),
+                commands::kqt::kqt(),
+                commands::kqt::kqt_cm(),
+                commands::kqt::kqt_cm_ai_hoi_bo_truong(),
+            ],
+            on_error: |error: FrameworkError<Data, Error>| {
+                Box::pin(async move {
+                    match error {
+                        poise::FrameworkError::ArgumentParse { error, .. } => {
+                            match error.downcast_ref::<SerenityError>() {
+                                Some(e) => error!("error: {:#?}", e),
+                                None => error!("unknown error: {:#?}", error),
+                            }
+                        }
+                        other => match poise::builtins::on_error(other).await {
+                            Ok(_) => (),
+                            Err(e) => error!("fatal error: {}", e),
+                        },
+                    }
+                })
+            },
             ..Default::default()
         })
-        .token(env::var("DISCORD_TOKEN").unwrap())
-        .intents(
-            serenity::GatewayIntents::non_privileged() | serenity::GatewayIntents::MESSAGE_CONTENT,
-        )
         .setup(move |ctx, _ready, framework| {
             Box::pin(async move {
                 poise::builtins::register_globally(ctx, &framework.options().commands).await?;
                 Ok(Data)
             })
-        });
+        })
+        .build();
 
-    framework.run().await.unwrap();
+    let client = ClientBuilder::new(
+        &env::var("DISCORD_TOKEN").expect("DISCORD_TOKEN is not set"),
+        GatewayIntents::non_privileged()
+            | GatewayIntents::GUILD_MESSAGES
+            | GatewayIntents::DIRECT_MESSAGES
+            | GatewayIntents::MESSAGE_CONTENT,
+    )
+    .framework(framework)
+    .await;
+
+    if let Err(e) = client.unwrap().start().await {
+        error!("error: {:#?}", e);
+        std::process::exit(1);
+    }
 
     Ok(())
 }
