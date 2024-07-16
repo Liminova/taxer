788cbca822403dc8c3f2bb0f38319862e6d8d246
Delnegend <kiennguyen19323@gmail.com>
2024-07-16 13:39:55 +0700

    refactor(commands/ping.rs): get latency from shard manager; change command desc

--- src/commands/ping.rs
+++ src/commands/ping.rs
@@ -5,31 +5,52 @@ use poise::{
 
 use crate::{Context, Error};
 
-/// A ping command.
+/// Check the App's status and latency
 #[poise::command(prefix_command, slash_command)]
 pub async fn ping(ctx: Context<'_>) -> Result<(), Error> {
-    let start = std::time::Instant::now();
-    let msg = ctx.say("Calculating latency...").await?;
+    let latency = ctx
+        .data()
+        .shard_manager
+        .clone()
+        .runners
+        .lock()
+        .await
+        .get(&ctx.serenity_context().shard_id)
+        .and_then(|runner| runner.latency);
 
-    let end = start.elapsed();
-    let latency = end.as_millis();
-
-    msg.edit(
-        ctx,
-        CreateReply::default()
-            .embed(
+    if let Some(latency) = latency {
+        ctx.send(
+            CreateReply::default().embed(
                 CreateEmbed::new()
                     .title("Pong!")
-                    .description(format!("Latency: {}ms", latency))
+                    .description(format!("Latency: {}ms", latency.as_millis()))
                     .color(0x00ff00)
                     .footer(CreateEmbedFooter::new(format!(
                         "Rustc version: {}",
-                        rustc_version_runtime::version(),
+                        rustc_version_runtime::version()
                     ))),
-            )
-            .ephemeral(false),
+            ),
+        )
+        .await
+        .map_err(|e| format!("commands::ping: can't send message: {}", e))?;
+
+        return Ok(());
+    }
+
+    ctx.send(
+        CreateReply::default().embed(
+            CreateEmbed::new()
+                .title("Pong!")
+                .description("Try again later")
+                .color(0xff3300)
+                .footer(CreateEmbedFooter::new(format!(
+                    "Rustc version: {}",
+                    rustc_version_runtime::version()
+                ))),
+        ),
     )
-    .await?;
+    .await
+    .map_err(|e| format!("commands::ping: can't send message: {}", e))?;
 
     Ok(())
 }
