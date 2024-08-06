cf4bc85483a84a24c8d9f4c16a0b61c75fb5bb5f
Delnegend <kiennguyen19323@gmail.com>
2024-08-06 07:36:57 +0700

    feat(queue.rs): new non-empty queue handler

--- src/commands/player/queue.rs
+++ src/commands/player/queue.rs
@@ -43,11 +43,65 @@ pub async fn queue(ctx: Context<'_>) -> Result<(), AppError> {
         }
     };
 
+    // get playing track id
+    let playing_track_id: Option<Uuid> = 'scoped: {
+        let songbird_manager = match songbird::get(ctx.serenity_context()).await {
+            Some(songbird_manager) => songbird_manager.clone(),
+            _ => {
+                return Err(AppError::from(anyhow!(
+                    "commands::player::queue: songbird not loaded"
+                )))
             }
         };
+        let call = match songbird_manager.get(guild_id) {
+            Some(call) => call,
+            _ => break 'scoped None,
+        };
+        let track_handle = call.lock().await.queue().current();
 
+        // make sure it's actually playing
+        if let Some(track_handle) = track_handle {
+            let is_playing = match track_handle.get_info().await {
+                Ok(info) => info.playing == PlayMode::Play,
+                Err(_) => break 'scoped None,
+            };
+            if is_playing {
+                break 'scoped Some(track_handle.uuid());
+            }
         }
+        None
+    };
 
+    // create the embed
+    let mut thumbnail = None;
+    let mut embed = CreateEmbed::default().title("Queue").fields(
+        playlist
+            .iter()
+            .map(|track_info| {
+                (
+                    format!(
+                        "{}{}",
+                        match track_info.id == playing_track_id.unwrap_or_default() {
+                            true => {
+                                thumbnail.clone_from(&track_info.thumbnail);
+                                "▶️  "
+                            }
+                            false => "",
+                        },
+                        track_info.get_title()
+                    ),
+                    format!(
+                        "{} | [Source]({})",
+                        track_info.get_pretty_description(),
+                        track_info.url
+                    ),
+                    false,
+                )
+            })
+            .collect::<Vec<_>>(),
+    );
+    if let Some(thumbnail) = thumbnail {
+        embed = embed.thumbnail(thumbnail);
     }
 
     if let Err(e) = ctx
