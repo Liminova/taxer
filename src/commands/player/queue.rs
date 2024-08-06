229b3cf684d7fa4e186fadbc22c0337680730b96
Delnegend <kiennguyen19323@gmail.com>
2024-08-06 07:36:19 +0700

    refactor(queue.rs): rm old non-empty queue handler

--- src/commands/player/queue.rs
+++ src/commands/player/queue.rs
@@ -43,82 +43,11 @@ pub async fn queue(ctx: Context<'_>) -> Result<(), AppError> {
         }
     };
 
-    if let Some(playlist) = playlist {
-        // get playing track info
-        let playing_track_id: Option<Uuid> = 'scoped: {
-            let songbird_manager = match songbird::get(ctx.serenity_context()).await {
-                Some(songbird_manager) => songbird_manager.clone(),
-                _ => {
-                    return Err(AppError::from(anyhow!(
-                        "commands::player::queue: songbird not loaded"
-                    )))
-                }
-            };
-            let call = match songbird_manager.get(guild_id) {
-                Some(call) => call,
-                _ => break 'scoped None,
-            };
-            let track_handle = call.lock().await.queue().current();
-
-            // make sure it's actually playing
-            if let Some(track_handle) = track_handle {
-                let is_playing = match track_handle.get_info().await {
-                    Ok(info) => info.playing == PlayMode::Play,
-                    Err(_) => break 'scoped None,
-                };
-                if is_playing {
-                    break 'scoped Some(track_handle.uuid());
-                }
             }
-            None
         };
 
-        // for adding to the embed later if is_some()
-        let mut thumbnail = None;
-
-        // create the embed
-        let mut embed = CreateEmbed::default().title("Queue").fields(
-            playlist
-                .iter()
-                .map(|track_info| {
-                    let title = match track_info.id == playing_track_id.unwrap_or_default() {
-                        true => {
-                            thumbnail.clone_from(&track_info.thumbnail);
-                            format!("▶️  {}", track_info.get_title())
-                        }
-                        false => track_info.get_title(),
-                    };
-
-                    let description = {
-                        let author = track_info
-                            .uploader
-                            .clone()
-                            .or_else(|| track_info.artist.clone())
-                            .unwrap_or_else(|| "Unknown".to_string());
-
-                        let duration = {
-                            let duration_in_sec = track_info.duration_in_sec;
-                            let hours = duration_in_sec / 3600;
-                            let minutes = (duration_in_sec % 3600) / 60;
-                            let seconds = duration_in_sec % 60;
-                            format!("{:02}:{:02}:{:02}", hours, minutes, seconds)
-                        };
-
-                        format!("{} | {} | [Source]({})", author, duration, track_info.url)
-                    };
-
-                    (title, description, false)
-                })
-                .collect::<Vec<_>>(),
-        );
-
-        if let Some(thumbnail) = thumbnail {
-            embed = embed.thumbnail(thumbnail);
         }
 
-        let _ = ctx.send(CreateReply::default().embed(embed)).await;
-
-        return Ok(());
     }
 
     if let Err(e) = ctx
